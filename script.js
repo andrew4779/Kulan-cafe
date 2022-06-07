34f5b288b2a2de6070144964d9e4f0f6a409efd5
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-06-07 18:30:52 +0300

    Commit all the files

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ document.querySelector('#menu-btn').onclick = () =>{
     cartItem.classList.remove('active');
 }
 
+
+
 let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () =>{
